fb19c0afadfbe4b2dedac87d254cb3dac5d65ecd
karanchalke <102895759+karanchalke@users.noreply.github.com>
2022-06-23 12:25:46 +0530

    Add files via upload

---
+++ 220340520045/exam database.sql
@@ -0,0 +1,9 @@
+use exam;
+create table book(bookid int, bookname varchar(15), price int);
+insert into book values(01, 'SOM', 999);
+insert into book values(02, 'Thermo', 1499);
+insert into book values(03, 'TOM', 1999);
+insert into book values(04, 'KOM', 799);
+insert into book values(05, 'Eng drawing', 2999);
+insert into book values(06, 'ICE', 1299);
+select * from book;
